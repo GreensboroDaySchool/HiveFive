7cef783ecdbe95a6ab01df923f0b5e6c25fe88ca
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-27 19:41:32 -0400

    Create HexNode class

---
+++ HiveFive/HexNode.swift
@@ -0,0 +1,12 @@
+//
+//  HexNode.swift
+//  Hive Five
+//
+//  Created by Jiachen Ren on 3/27/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import Foundation
+class HexNode {
+    
+}
