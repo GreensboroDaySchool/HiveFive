dae9d319d27ef1f498763d4dd8ab9b1c4ed44e2b
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-28 18:55:26 -0400

    Create NodeProtocol

--- HiveFive/Model/HexNode.swift
+++ HiveFive/Model/HexNode.swift
@@ -11,16 +11,69 @@ import Foundation
 /**
  This is the parent of Hive, QueenBee, Beetle, Grasshopper, Spider, and SoldierAnt, since all of them are pieces that together consist a hexagonal board.
  */
-class HexNode {
-    var north: HexNode?
-    var northWest: HexNode?
-    var northEast: HexNode?
-    var south: HexNode?
-    var southWest: HexNode?
-    var southEast: HexNode?
-    
-    init() {
-        
+class HexNode: NodeProtocol {
+    var nodes = [HexNode?](repeating: nil, count: 6)
+
+    var north: HexNode? {
+        get {
+            return nodes[0]
+        }
+        set {
+            nodes[0] = newValue
+        }
     }
-    
+    var northWest: HexNode? {
+        get {
+            return nodes[1]
+        }
+        set {
+            nodes[1] = newValue
+        }
+    }
+    var northEast: HexNode? {
+        get {
+            return nodes[2]
+        }
+        set {
+            nodes[2] = newValue
+        }
+    }
+    var south: HexNode? {
+        get {
+            return nodes[3]
+        }
+        set {
+            nodes[3] = newValue
+        }
+    }
+    var southWest: HexNode? {
+        get {
+            return nodes[4]
+        }
+        set {
+            nodes[4] = newValue
+        }
+    }
+    var southEast: HexNode? {
+        get {
+            return nodes[5]
+        }
+        set {
+            nodes[5] = newValue
+        }
+    }
+
+    init() {
+
+    }
+
+}
+
+protocol NodeProtocol {
+    var north: HexNode? { get set }
+    var northEast: HexNode? {get set}
+    var northWest: HexNode? {get set}
+    var south: HexNode? {get set}
+    var southEast: HexNode? {get set}
+    var southWest: HexNode? {get set}
 }
