f1a7087d234b5787a1d7d5e972a7543606c28ea8
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-28 11:25:15 -0400

    Create Beetle, Grasshopper, QueenBee, SoldierAnt, and Spider classes

---
+++ HiveFive/Model/Grasshopper.swift
@@ -0,0 +1,12 @@
+//
+//  Grasshopper.swift
+//  Hive Five
+//
+//  Created by Jiachen Ren on 3/28/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import Foundation
+class Grasshopper: HexNode {
+    
+}
