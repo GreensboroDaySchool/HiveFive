f2a7c1600a9a232716e24e15fb85c39e96538ed7
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-28 11:19:20 -0400

    Create Hive class; move Hive, HexNode under Model folder

---
+++ HiveFive/Model/HexNode.swift
@@ -0,0 +1,22 @@
+//
+//  HexNode.swift
+//  Hive Five
+//
+//  Created by Jiachen Ren on 3/27/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import Foundation
+class HexNode {
+    var north: HexNode?
+    var northWest: HexNode?
+    var northEast: HexNode?
+    var south: HexNode?
+    var southWest: HexNode?
+    var southEast: HexNode?
+    
+    init() {
+        
+    }
+    
+}
--- HiveFive/Model/Hive.swift
+++ HiveFive/Model/Hive.swift
@@ -1,12 +1,9 @@
 //
-//  HexNode.swift
+//  Hive.swift
 //  Hive Five
 //
-//  Created by Jiachen Ren on 3/27/18.
+//  Created by Jiachen Ren on 3/28/18.
 //  Copyright © 2018 Greensboro Day School. All rights reserved.
 //
 
 import Foundation
-class HexNode {
-    
-}
