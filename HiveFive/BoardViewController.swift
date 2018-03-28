c60ea8b8f611391046b1dab9602a758a6bc29d2c
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-28 11:46:53 -0400

    Create BoardView, BoardViewController

---
+++ HiveFive/BoardViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BoardViewController.swift
+//  Hive Five
+//
+//  Created by Jiachen Ren on 3/28/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import UIKit
+/**
+ View Controller class of BoardView
+ */
+class BoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HiveFive/View/BoardView.swift
@@ -0,0 +1,24 @@
+//
+//  BoardView.swift
+//  Hive Five
+//
+//  Created by Jiachen Ren on 3/28/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import UIKit
+/**
+ The Board class takes a Hive structure and displays it.
+ */
+class BoardView: UIView {
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
+
