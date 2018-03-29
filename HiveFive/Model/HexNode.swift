3f738667272e7d69598e3cf76ac01217e46e1006
Marcus Zhou <other.marcus@icloud.com>
2018-03-28 20:06:44 -0400

    Emoji😀

--- HiveFive/Model/HexNode.swift
+++ HiveFive/Model/HexNode.swift
@@ -22,69 +22,24 @@ import Foundation
 /**
  This is the parent of Hive, QueenBee, Beetle, Grasshopper, Spider, and SoldierAnt, since all of them are pieces that together consist a hexagonal board.
  */
-class HexNode: NodeProtocol {
-    var nodes = [HexNode?](repeating: nil, count: 6)
-
-    var north: HexNode? {
-        get {
-            return nodes[0]
-        }
-        set {
-            nodes[0] = newValue
-        }
+class HexNode {
+    
+    /**
+           _____(up)_____
+          /              \
+     (upLeft)         (upRight)
+        /     Oh Yeah!     \
+        \      Hive        /
+    (downLeft)       (downRight)
+          \____(down)____/
+     */
+    struct SurroundingNodes {
+        var up: HexNode?
+        var upRight: HexNode?
+        var upLeft: HexNode?
+        var downRight: HexNode?
+        var downLeft: HexNode?
     }
-    var northWest: HexNode? {
-        get {
-            return nodes[1]
-        }
-        set {
-            nodes[1] = newValue
-        }
-    }
-    var northEast: HexNode? {
-        get {
-            return nodes[2]
-        }
-        set {
-            nodes[2] = newValue
-        }
-    }
-    var south: HexNode? {
-        get {
-            return nodes[3]
-        }
-        set {
-            nodes[3] = newValue
-        }
-    }
-    var southWest: HexNode? {
-        get {
-            return nodes[4]
-        }
-        set {
-            nodes[4] = newValue
-        }
-    }
-    var southEast: HexNode? {
-        get {
-            return nodes[5]
-        }
-        set {
-            nodes[5] = newValue
-        }
-    }
-
-    init() {
-
-    }
-
-}
-
-protocol NodeProtocol {
-    var north: HexNode? { get set }
-    var northEast: HexNode? {get set}
-    var northWest: HexNode? {get set}
-    var south: HexNode? {get set}
-    var southEast: HexNode? {get set}
-    var southWest: HexNode? {get set}
+    
+    
 }
