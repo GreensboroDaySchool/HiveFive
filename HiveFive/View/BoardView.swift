819f5147c8668b115fe29f0a2aa7edd62599bce0
Marcus Zhou <other.marcus@icloud.com>
2018-03-28 22:41:41 -0400

    Add board coordination transformation algo

--- HiveFive/View/BoardView.swift
+++ HiveFive/View/BoardView.swift
@@ -19,9 +19,12 @@
 
 import UIKit
 
-let nodeRadius = 16.0
+let nodeRadius: CGFloat = 16.0
 
+//This is used to store the processed coordination
 fileprivate typealias BoardCoordinationPair = (node: HexNode, coordination: CGPoint)
+//This is used to queue the pair into the line for later processes
+fileprivate typealias QueuedNodePair = (node: HexNode, sourceCoordination: CGPoint, sourceDirection: Direction)
 
 class BoardView: UIView {
     var hive: Hive?
@@ -33,16 +36,52 @@ class BoardView: UIView {
 
 extension BoardView {
     fileprivate func layoutHive() -> [BoardCoordinationPair]? {
+        //Only layout when there is a hive
         guard let hive = hive else { return nil }
-        var fifo = [HexNode]() //A queue that stores all the nodes that need to be processed
+        var fifo = [QueuedNodePair]() //A queue that stores all the nodes that need to be processed
         var processed = [BoardCoordinationPair]()
         
-        var current = BoardCoordinationPair(hive.root, CGPoint(x: 0, y: 0))
-        processed.append(current)
+        let directions: [Direction] = [.up, .upLeft, .upRight, .down, .downLeft, .downRight]
         
+        //Assign (0, 0) to the root node
+        processed.append(BoardCoordinationPair(hive.root, CGPoint(x: 0, y: 0)))
+        
+        fifo.append(contentsOf: processed.first!
+            .node
+            .neighbors
+            .nodes//all the neighboring nodes of the root cell
+            .enumerated()
+            .filter({ $0.element !== nil })//Filter out nil nodes
+            .map({ QueuedNodePair($0.element!, CGPoint(x: 0, y: 0), directions[$0.offset]) }))//map to our QueuedNodePair tuple
+        
+        //Transform node locations from the inside to the outside
         while(fifo.count > 0){
             let current = fifo.removeFirst()
+            var transformed = current.sourceCoordination
             
+            switch(current.sourceDirection){
+            case .upRight, .downRight: transformed.x += nodeRadius * 0.5
+            case .upLeft, .downLeft: transformed.x -= nodeRadius * 0.5
+            default: break
+            }
+            
+            switch(current.sourceDirection){
+            case .upRight, .upLeft: transformed.y += nodeRadius * sin(.pi / 3)
+            case .downLeft, .downRight: transformed.y -= nodeRadius * sin(.pi / 3)
+            case .up: transformed.y += nodeRadius * sin(.pi / 3) * 2
+            case .down: transformed.y -= nodeRadius * sin(.pi / 3) * 2
+            }
+            
+            processed.append(BoardCoordinationPair(current.node, transformed))
+            
+            //Append current node's neighboors
+            fifo.append(contentsOf: processed.last!
+                .node
+                .neighbors
+                .nodes//all the neighboring nodes of the root cell
+                .enumerated()
+                .filter({ n in n.element !== nil && !processed.contains{ n.element?.neighbors.equals($0.node.neighbors) == true } })//Filter out nil nodes and nodes that have been processed
+                .map({ QueuedNodePair($0.element!, CGPoint(x: 0, y: 0), directions[$0.offset]) }))
         }
         
         return processed
