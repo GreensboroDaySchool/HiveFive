2f22927c4323d0c8d7631d78e39e176ef7ea658a
Marcus Zhou <other.marcus@icloud.com>
2018-03-30 10:00:52 -0400

    Create NodeView

---
+++ HiveFive/View/NodeView.swift
@@ -0,0 +1,16 @@
+//
+//  NodeView.swift
+//  Hive Five
+//
+//  Created by Marcus Zhou on 3/30/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import UIKit
+
+class NodeView: UIView {
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+}
