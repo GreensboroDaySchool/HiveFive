84311fd740eebc3e33c19e8f46afcfbff91127dc
Jiachen Ren <arc4jiachenren@gmail.com>
2018-04-05 11:10:40 -0400

    Add lib SlideMenuController

---
+++ HiveFive/Controller/ContainerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ContainerViewController.swift
+//  Hive Five
+//
+//  Created by Jiachen Ren on 4/5/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: SlideMenuController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func awakeFromNib() {
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "Main") {
+            self.mainViewController = controller
+        }
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "Left") {
+            self.leftViewController = controller
+        }
+        super.awakeFromNib()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
