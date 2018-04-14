644e91a5d9ee871199a8ac272aedd574d2a9c5bd
Marcus Zhou <other.marcus@icloud.com>
2018-04-14 18:03:17 -0400

    Add new target for multiplayer server

---
+++ HiveFive Server/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HiveFive Server
+//
+//  Created by Marcus Zhou on 4/14/18.
+//  Copyright © 2018 Greensboro Day School. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
